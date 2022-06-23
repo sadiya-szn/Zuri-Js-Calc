f1538c7a3b58aaa5b7d739ba39f0bebcd0519b00
sadiya <104234800+sadiya-szn@users.noreply.github.com>
2022-06-23 11:20:36 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var opr = prompt("Operator: ")
+var num1 = parseInt(prompt("Number 1: "))
+var num2 = parseInt(prompt("Number 2: "))
+
+if(opr == "+")
+{
+    console.log(num1 + num2)
+} else if(opr == "-")
+{
+    console.log(num1 - num2)
+} else if (opr == "*")
+{
+    console.log(num1 * num2)
+} else if (opr == "/")
+{
+    console.log(num1 / num2)
+}
